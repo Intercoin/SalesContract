158057b4e6e35f54d559030bc6392b3f3bcb5858
Artem Subbotin <subbotin.artem@gmail.com>
2025-01-09 18:39:45 +0200

    deployment scripts(minebtch)

---
+++ scripts/helpers/arguments-minebtch.js
@@ -0,0 +1,78 @@
+const helperUsdc = require('./usdcAddress.js');
+const helperWrapper = require('./wrapperAddress.js');
+function getArguments(){
+    return [
+        // * @param _commonSettings CommonSettings data struct
+        // *  address sellingToken address of ITR token
+        // *  address token0 USD Coin
+        // *  address token1 Wrapped token (WETH,WBNB,...)
+        // *  address liquidityLib liquidityLib address(see intercoin/liquidity pkg)
+        // *  uint64 endTime after this time exchange stop
+        [
+            "0x49746C1BFBA595987623091671D77537A9F64b59", // minebtch traded token
+            helperUsdc.getUSDCAddress(),//"0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+            helperWrapper.getWrapperAddress(),//"0x55d398326f99059ff775485246999027b3197955",
+            "0x1ea4c4613a4dfdaeeb95a261d11520c90d5d6252",
+            1798761600  // 1 Jan 2027
+        ],
+        // * @param _priceSettings PriceSettings struct's array
+        // *  uint64 timestamp timestamp
+        // *  uint256 price price exchange
+        // *  uint256 amountRaised raised amount 
+        [
+            //0.05$ - first 10 mil
+            //0.10$ - 10-20 mil
+            //0.20$ - 20-30 mil
+            //0.40$ - 30-40 mil
+            //1.00$ - 40-50 mil
+            [1719792000, 500000000, "0"],
+            [0, 1000000000, "0x845951614014880000000"],
+            [0, 2000000000, "0x108b2a2c28029100000000"],
+            [0, 4000000000, "0x18d0bf423c03d900000000"],
+            [0, 10000000000, "0x2116545850052200000000"]
+        ],
+        // * @param _bonusSettings ThresholdBonuses struct's array
+        // *  uint256 threshold thresholds
+        // *  uint256 bonus bonuses
+        //   
+        // dividing by 0.05$ to get amount of tokens
+        // $0-10K 0% bonus
+        // $10K - $25K 10% bonus
+        // $25K - $50K 25% bonus
+        // $50K - $75K 50% bonus 
+        // $75K - $100K 75% bonus
+        // $100K+     100% bonus m
+
+        [   //reach 0 tokens - 0 bonus, reach 200_000 tokens - 10% and so on
+            [0,0],                          // 0 tokens - 0%
+            ["0x2a5a058fc295ed000000",10],  //200_000 tokens - 10%
+            ["0x69e10de76676d0800000",25],  //500_000
+            ["0xd3c21bcecceda1000000",50],  //1_000_000
+            ["0x13da329b6336471800000",75], //1_500_000
+            ["0x1a784379d99db42000000",100] // 2_000_000
+        ],
+        // * @param _ownerCanWithdraw enum option where:
+        // *  0 -owner can not withdraw tokens
+        // *  1 -owner can withdraw tokens only after endTimePassed
+        // *  2 -owner can withdraw tokens anytime
+            2,
+        // * @param _whitelistData whitelist data struct
+        // *  address contractAddress;
+        // *	bytes4 method;
+        // *	uint8 role;
+        ["0x0000000000000000000000000000000000000000","0x00000000",0,true],
+        // * @param _lockedInPrice lockedInPrice struct
+        // *  uint256 _minimumLockedInAmount Minimum amount required to buy and hold the price.
+        // *  uint256 _maximumLockedInAmount Maximum amount available to buy at the held price.
+        // Well, in that case let’s make it 100,000 tokens and 3 million tokens
+        ["0x152d02c7e14af6000000","0x27b46536c66c8e0000000"],
+        // * @param _compensationSettings compensationSettings data struct		
+        // *  uint64 endTime after this time receiving compensation tokens will be disabled
+        [   // 1 July 2025
+            1751328000
+        ]
+    ];
+}
+module.exports = {
+    getArguments
+}
---
+++ scripts/produce-for-minebtch.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+const { 
+    getArguments
+} = require("./helpers/arguments-minebtch.js");
+
+async function main() {
+	
+	
+	var depl_local,
+        depl_auxiliary,
+        depl_releasemanager,
+		depl_sales;
+
+    var signers = await ethers.getSigners();
+    if (signers.length == 1) {
+        depl_local = signers[0];
+        depl_auxiliary = signers[0];
+        depl_releasemanager = signers[0];
+		depl_sales = signers[0];
+    } else {
+        [
+            depl_local,
+            depl_auxiliary,
+            depl_releasemanager,
+			depl_sales
+        ] = signers;
+    }
+
+	console.log(
+		"Deploying contracts with the account:",
+		depl_auxiliary.address
+	);
+
+	const _arguments = getArguments();
+
+	console.log(_arguments);
+    const deployerBalanceBefore = await ethers.provider.getBalance(depl_auxiliary.address);
+    console.log("Account balance:", (deployerBalanceBefore).toString());
+
+    //const SalesFactory = await ethers.getContractAt("SalesFactory", "0x080101006379fcb14e08a24388888bbb8fbfa8c9");   
+	const SalesFactoryF = await ethers.getContractFactory("SalesFactory");   
+	const SalesFactory = SalesFactoryF.attach("0x08010100d9d250947417b9de5b6622fa1dcb64b8");
+	//---
+	
+	let tx = await SalesFactory.connect(depl_auxiliary).produceWithStablePrices(..._arguments);
+	
+	const rc = await tx.wait(); // 0ms, as tx is already confirmed
+
+	const event = rc.logs.find(obj => obj.fragment && obj.fragment.name === 'InstanceCreated');
+	const [instance,] = event.args;
+	console.log("instance = ", instance);
+
+	const newOwner = '0x3BB042aD28d16a9c62fd1b7f33615308ACB1fC61';
+	console.log("transferOwnership to ", newOwner);
+	const SalesWithStablePricesF = await ethers.getContractFactory("SalesWithStablePrices");   
+	const SalesWithStablePrices = SalesWithStablePricesF.attach(instance);
+	await SalesWithStablePrices.connect(depl_auxiliary).transferOwnership(newOwner);
+	console.log("Done");
+
+
+	const deployerBalanceAfter = await ethers.provider.getBalance(depl_auxiliary.address);
+	console.log("Spent:", ethers.formatUnits(deployerBalanceBefore - deployerBalanceAfter), 18);
+	console.log("gasPrice:", ethers.formatUnits((await network.provider.send("eth_gasPrice")), "gwei")," gwei");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+	console.error(error);
+	process.exit(1);
+  });
